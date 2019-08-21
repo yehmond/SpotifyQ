189f766903075e6176d368ee3d1d87ffe97cb912
yehmond <yehmondray@gmail.com>
2019-08-20 17:12:05 -0700

    implemented voting

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -121,8 +121,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
