8f527109e24a3d702b212729c33e99bd11f3efdd
yehmond <yehmondray@gmail.com>
2019-08-21 21:27:46 -0700

    fixed css

--- Spotify/settings.py
+++ Spotify/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Spotify.urls'
@@ -118,6 +119,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
